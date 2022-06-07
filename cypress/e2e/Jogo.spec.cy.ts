55c2af6fc9278bae24ec1f3d6a4da789bdca6278
Rodrigo Henrique <rodhenrc@outlook.com>
2022-06-07 15:59:15 -0300

    Pequena mudança nos testes para detectar que os quadrados estão vazios

--- cypress/e2e/Jogo.spec.cy.ts
+++ cypress/e2e/Jogo.spec.cy.ts
@@ -3,8 +3,10 @@
 describe("playing", () => {
   it("players should win or draw", () => {
     cy.visit("/");
+    cy.get('[data-cy="quadrados"] span').should("have.text", "");
     cy.playerOneWin();
     cy.get('[data-cy="jogar-novamente"]').click();
+    cy.get('[data-cy="quadrados"] span').should("have.text", "");
     cy.playerTwoWin();
     cy.get('[data-cy="jogar-novamente"]').click();
     cy.draw();
